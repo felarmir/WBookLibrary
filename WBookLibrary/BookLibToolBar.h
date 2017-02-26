d4c2701d3b2343e76d45e3688499da605b13e462
wiirlock <wiirlock@gmail.com>
2017-02-26 22:21:05 +0300

    Open book on single window and add toolbar item

---
+++ WBookLibrary/BookLibToolBar.h
@@ -0,0 +1,13 @@
+//
+//  BookLibToolBar.h
+//  WBookLibrary
+//
+//  Created by Denis Andreev on 26/02/2017.
+//  Copyright © 2017 Denis Andreev. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface BookLibToolBar : NSToolbar
+
+@end
---
+++ WBookLibrary/BookLibToolBar.m
@@ -0,0 +1,13 @@
+//
+//  BookLibToolBar.m
+//  WBookLibrary
+//
+//  Created by Denis Andreev on 26/02/2017.
+//  Copyright © 2017 Denis Andreev. All rights reserved.
+//
+
+#import "BookLibToolBar.h"
+
+@implementation BookLibToolBar
+
+@end
